9dac371940a050f6d1350cca45250ef9d552fd00
sogo.komatsuzaki.xx <sogo.komatsuzaki.xx@ambl.co.jp>
2025-06-03 18:07:43 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,33 @@
-import { GetStaticProps } from 'next';
-import { client } from '@/libs/contentful';
-import { Entry} from 'contentful';
-import type { PostSkeleton } from '@/types/post'
+// import { GetStaticProps } from 'next';
+// import { client } from '@/libs/contentful';
+// import { Entry} from 'contentful';
+// import type { PostSkeleton } from '@/types/post'
 import { BaseHeadingLevel1 } from '@/components/BaseHeadingLevel1';
-import { PostList } from '@/components/PostList';
+// import { PostList } from '@/components/PostList';
 
-interface Props {
-  posts: Entry<PostSkeleton>[];
-}
+// interface Props {
+//   posts: Entry<PostSkeleton>[];
+// }
 
-export default function Home({ posts }: Props) {
+// export default function Home({ posts }: Props) {
+export default function Home() {
   return (
     <main>
       <BaseHeadingLevel1>記事一覧</BaseHeadingLevel1>
-      <PostList posts={posts} />
+      {/* <PostList posts={posts} /> */}
     </main>
   );
 }
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const response = await client.getEntries<PostSkeleton>({
-    content_type: 'post',
-    order: ['-sys.createdAt'],
-  });
+// export const getStaticProps: GetStaticProps<Props> = async () => {
+//   const response = await client.getEntries<PostSkeleton>({
+//     content_type: 'post',
+//     order: ['-sys.createdAt'],
+//   });
 
-  return {
-    props: {
-      posts: response.items,
-    },
-  };
-};
+//   return {
+//     props: {
+//       posts: response.items,
+//     },
+//   };
+// };
